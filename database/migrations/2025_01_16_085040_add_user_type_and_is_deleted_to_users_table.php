6392a60a01e53f9adb8e943e1cc58cc157a67ec4
yesenvidnath <ykandalama@gmail.com>
2025-01-16 14:41:23 +0530

    The Database is finalizedT

---
+++ database/migrations/2025_01_16_085040_add_user_type_and_is_deleted_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('user_type', ['Admin', 'ContentWriter', 'SEO Analyst'])->default('ContentWriter')->after('email');
+            $table->boolean('is_deleted')->default(0)->after('user_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['user_type', 'is_deleted']);
+        });
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use Illuminate\Support\Facades\Hash;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -16,8 +17,27 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'user_type' => 'Admin',
+            'is_deleted' => 0,
+        ]);
+
+        User::factory()->create([
+            'name' => 'Content Writer',
+            'email' => 'writer@example.com',
+            'password' => Hash::make('password'),
+            'user_type' => 'ContentWriter',
+            'is_deleted' => 0,
+        ]);
+
+        User::factory()->create([
+            'name' => 'SEO Analyst',
+            'email' => 'seo@example.com',
+            'password' => Hash::make('password'),
+            'user_type' => 'SEO Analyst',
+            'is_deleted' => 0,
         ]);
     }
 }
