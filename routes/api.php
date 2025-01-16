11ab8fbf9893a5174ed338d511125e6bceb21708
yesenvidnath <ykandalama@gmail.com>
2025-01-16 14:20:24 +0530

    all the controllers has been created successfully

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Common\SeoController;
+use App\Http\Controllers\Common\PageController;
+use App\Http\Controllers\Common\NavMenuController;
+use App\Http\Controllers\Common\GalleryController;
+use App\Http\Controllers\Common\CategoryController;
+use App\Http\Controllers\Common\MenuItemController;
+use App\Http\Controllers\Common\EmailListController;
+use App\Http\Controllers\Common\ImageListController;
+use App\Http\Controllers\Common\GlobalInfoController;
+use App\Http\Controllers\Common\LocationsListController;
+use App\Http\Controllers\Common\PhoneNumbersListController;
